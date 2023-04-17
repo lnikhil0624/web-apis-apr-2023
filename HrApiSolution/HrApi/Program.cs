371ba66281f15d0d13ce7f7cb2fa6ef39aae4cb8
lnikhil0624 <nikhil_lothumalla_2@progressive.com>
2023-04-17 13:51:24 -0400

    Additions of Entity Framework and other Docker changes.

--- HrApiSolution/HrApi/Program.cs
+++ HrApiSolution/HrApi/Program.cs
@@ -1,3 +1,6 @@
+using HrApi.Domain;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,18 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var hrConnectionString = builder.Configuration.GetConnectionString("hr-data");
+
+if (hrConnectionString is null)
+{
+    throw new Exception("No Connection String for HR Database");
+}
+
+builder.Services.AddDbContext<HrDataContext>(options =>
+{
+    options.UseSqlServer(hrConnectionString);
+});
 
 var app = builder.Build();
 
